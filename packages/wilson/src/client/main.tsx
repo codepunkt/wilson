ae45ce15229a4c3fb62d3345be580b24826f7191
Christoph Werner <codepunkt@users.noreply.github.com>
2021-07-28 07:01:19 +0200

    refactor: simplify trailing slash link vnode hook (#107)

--- packages/wilson/src/client/main.tsx
+++ packages/wilson/src/client/main.tsx
@@ -19,20 +19,21 @@ const vnodeHook = ({
     type === 'a' &&
     props &&
     props.href &&
-    !props.href.startsWith('http') &&
-    !props.href.startsWith('#') &&
-    (!props.target || props.target === '_self')
+    !props.href.includes('#') &&
+    !props.href.match(/^(http|https|mailto|tel):/) &&
+    (!props.target || props.target === '_self') &&
+    !props.href.endsWith('/')
   ) {
-    if (!props.href.includes('#') && !props.href.endsWith('/')) {
-      console.error(
-        '%s%c%s%c%s',
-        'Warning: "href" attributes of relative links should end with a trailing slash. Please fix ',
-        'color:maroon;',
-        `<a href="${props.href}">${props.children}</a>. `,
-        'color:inherit;',
-        'See https://wilsonjs.com/docs/faq#trailing-slashes for more information.'
-      )
-    }
+    console.error(
+      '%s%c%s%c%s',
+      'Warning: "href" attributes of relative links should end with a trailing slash. Please fix ',
+      'color:maroon;',
+      `<a href="${props.href}">${
+        typeof props.children === 'string' ? props.children : '...'
+      }</a>. `,
+      'color:inherit;',
+      'See https://wilsonjs.com/docs/faq#trailing-slashes for more information.'
+    )
   }
 }
 
